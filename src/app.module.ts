95eea8b4b2b86d9289010449d720c8a3fd67958d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-14 01:07:49 +0200

    Fix modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,24 @@ import { FramedataController } from './framedata/framedata.controller';
 import { FramedataService } from './framedata/framedata.service';
 import { FramedataModule } from './framedata/framedata.module';
 import { CharacterCodesController } from './characterCodes/characterCodes.controller';
-import { CharacterCodesService } from './characterCodes/characterCodes.service';
 import { CharacterCodesModule } from './characterCodes/characterCodes.module';
 import { GameCodesModule } from './gameCodes/gameCodes.module';
 import { GameCodesService } from './gameCodes/gameCodes.service';
 import { GameCodesController } from './gameCodes/gameCodes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FramedataModule, CharacterCodesModule, GameCodesModule],
+  imports: [
+    FramedataModule,
+    CharacterCodesModule,
+    GameCodesModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [
     FramedataController,
     CharacterCodesController,
     GameCodesController,
   ],
-  providers: [FramedataService, CharacterCodesService, GameCodesService],
+  providers: [FramedataService, GameCodesService],
 })
 export class AppModule {}
--- src/characterCodes/characterCodes.module.ts
+++ src/characterCodes/characterCodes.module.ts
@@ -1,4 +1,18 @@
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  CharacterCodes,
+  CharacterCodesSchema,
+} from './schemas/character-codes.schema';
 import { Module } from '@nestjs/common';
+import { CharacterCodesService } from './characterCodes.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: CharacterCodes.name, schema: CharacterCodesSchema },
+    ]),
+  ],
+  providers: [CharacterCodesService],
+  exports: [CharacterCodesService],
+})
 export class CharacterCodesModule {}
