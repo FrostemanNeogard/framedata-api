31dc9a56adec9f2b99ecfd787e6ba2e2e94a36d7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-14 18:16:35 +0200

    Change response type for gameCode

--- src/gameCodes/gameCodes.controller.ts
+++ src/gameCodes/gameCodes.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { GameCodesService } from './gameCodes.service';
-import { GameCode } from 'src/__types/gameCode';
 
 @Controller('gamecodes')
 export class GameCodesController {
   constructor(private readonly gameCodesService: GameCodesService) {}
 
   @Get()
-  public getAllGameCodes(): GameCode[] {
-    return this.gameCodesService.getAllGameCodes();
+  public async getAllGameCodes(): Promise<string[]> {
+    return await this.gameCodesService.getAllGameCodes();
   }
 }
