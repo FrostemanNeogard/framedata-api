28c9208c7cdd52d5a9f3156f6a46fccbc402b4a9
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-17 21:14:23 +0200

    Create framedataPatchDto

---
+++ src/framedata/dtos/framedataPatchDto.ts
@@ -0,0 +1,52 @@
+import { IsOptional, IsString, IsArray } from 'class-validator';
+import { FrameData } from 'src/__types/frameData';
+import { TekkenMoveCategory } from 'src/__types/moveCategories';
+
+export class FramedataPatchDto implements Partial<FrameData> {
+  @IsOptional()
+  @IsString()
+  input?: string;
+
+  @IsOptional()
+  @IsString()
+  hit_level?: string;
+
+  @IsOptional()
+  @IsString()
+  damage?: string;
+
+  @IsOptional()
+  @IsString()
+  startup?: string;
+
+  @IsOptional()
+  @IsString()
+  block?: string;
+
+  @IsOptional()
+  @IsString()
+  hit?: string;
+
+  @IsOptional()
+  @IsString()
+  counter?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  notes?: string[];
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  alternateInputs?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  categories?: TekkenMoveCategory[];
+}
