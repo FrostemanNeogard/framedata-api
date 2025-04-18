ac121f4e297894716a62f9f0ba1ebe34e03327d5
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-18 17:09:35 +0200

    Fix tests

--- src/suggestions/suggestions.service.spec.ts
+++ src/suggestions/suggestions.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SuggestionsService } from './suggestions.service';
+import { SuggestionsRepository } from './suggestions.repository';
 
 describe('SuggestionsService', () => {
   let service: SuggestionsService;
 
+  const mockRepository = {
+    findByCharacter: jest.fn(),
+    saveCharacter: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SuggestionsService],
+      providers: [
+        SuggestionsService,
+        {
+          provide: SuggestionsRepository,
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SuggestionsService>(SuggestionsService);
