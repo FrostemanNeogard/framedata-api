7398f546336def1e956d4c4c69304545ea95ae0e
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-24 17:50:00 +0200

    Remove guards

--- src/gameCodes/gameCodes.controller.ts
+++ src/gameCodes/gameCodes.controller.ts
@@ -1,13 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { GameCodesService } from './gameCodes.service';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('gamecodes')
 export class GameCodesController {
   constructor(private readonly gameCodesService: GameCodesService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
   public async getAllGameCodes(): Promise<string[]> {
     return this.gameCodesService.getAllGameCodes();
   }
