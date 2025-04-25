c1dea690e862307628fbaacffcb74f238ef6ae28
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-04-25 15:04:33 +0200

    Add type safety to configuration

--- src/__config/configuration.ts
+++ src/__config/configuration.ts
@@ -1,6 +1,10 @@
-export default () => ({
+import { ConfigFactory } from '@nestjs/config';
+
+const configuration: ConfigFactory = () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     uri: process.env.DATABASE_URI,
   },
 });
+
+export default configuration;
