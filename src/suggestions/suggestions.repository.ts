79f38606ef9b2089a20deef6d7c8a01ca93ba78d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-06-03 18:02:11 +0200

    Create method to delete a suggestion by id

--- src/suggestions/suggestions.repository.ts
+++ src/suggestions/suggestions.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Suggestion } from './entities/suggestion.entity';
@@ -21,5 +21,15 @@ export class SuggestionsRepository {
   async create(suggestion: Suggestion) {
     const created = new this.suggestionModel(suggestion);
     return created.save();
+  }
+
+  async deleteById(id: string) {
+    const suggestion = await this.findById(id);
+
+    if (!suggestion) {
+      throw new NotFoundException('Suggestion not found.');
+    }
+
+    return suggestion.deleteOne();
   }
 }
