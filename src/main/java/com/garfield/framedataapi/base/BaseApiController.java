0b318d9a6d9f3ddedb58050ef0ecdbcc5bc6fbec
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-09-24 22:04:43 +0200

    authenticate controllers by default

--- src/main/java/com/garfield/framedataapi/base/BaseApiController.java
+++ src/main/java/com/garfield/framedataapi/base/BaseApiController.java
@@ -1,9 +1,11 @@
 package com.garfield.framedataapi.base;
 
+import com.garfield.framedataapi.config.Authenticated;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URI;
 
+@Authenticated
 public abstract class BaseApiController {
 
     @Value("${server.servlet.context-path}")
