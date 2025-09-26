524fd452d982adc5c483f88068504cb59955213f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-09-26 18:42:01 +0200

    create staff decorator

---
+++ src/main/java/com/garfield/framedataapi/config/Staff.java
@@ -0,0 +1,13 @@
+package com.garfield.framedataapi.config;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@PreAuthorize("hasRole('ROLE_STAFF')")
+public @interface Staff {
+}
