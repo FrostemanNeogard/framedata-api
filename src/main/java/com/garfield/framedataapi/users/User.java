ae2fb729c69fd2a9287513e7020cfdb868f22da3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-09-26 18:42:28 +0200

    add profilepictureurl and accountcreatedat columns

--- src/main/java/com/garfield/framedataapi/users/User.java
+++ src/main/java/com/garfield/framedataapi/users/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
@@ -19,10 +20,17 @@ public class User {
     private UUID id;
 
     @Column
-    private String name;
+    private String username;
+
+    @Column
+    private String profilePictureUrl;
 
     @Column
     private String email;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date accountCreatedAt;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
