15ed6dfdcf67aa081f05ed9b589bcdf3ddb9cf15
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-09-27 11:33:42 +0200

    fix incorrect field name

--- src/main/java/com/garfield/framedataapi/users/dtos/UserDto.java
+++ src/main/java/com/garfield/framedataapi/users/dtos/UserDto.java
@@ -6,7 +6,7 @@ import com.garfield.framedataapi.users.User;
 import java.util.List;
 import java.util.UUID;
 
-public record UserDto(UUID id, String name, String email, List<RoleDto> roles) {
+public record UserDto(UUID id, String username, String email, List<RoleDto> roles) {
 
     public static List<UserDto> fromEntityList(List<User> users) {
         return users.stream().map(UserDto::fromEntity).toList();
@@ -15,7 +15,7 @@ public record UserDto(UUID id, String name, String email, List<RoleDto> roles) {
     public static UserDto fromEntity(User user) {
         return new UserDto(
                 user.getId(),
-                user.getName(),
+                user.getUsername(),
                 user.getEmail(),
                 user.getRoles().stream().map(RoleDto::fromEntity).toList()
         );
