e8d8b556a8a5708db13bd075e20f657d8799ffb3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-01 15:51:29 +0200

    add new exceptions

--- src/main/java/com/garfield/framedataapi/config/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/garfield/framedataapi/config/exceptionHandler/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.garfield.framedataapi.config.exceptionHandler;
 
+import com.garfield.framedataapi.aliases.exceptions.AmbiguousCharacterNameException;
 import com.garfield.framedataapi.config.structure.ApiResponse;
 import com.garfield.framedataapi.config.structure.ApiResponseEntity;
+import com.garfield.framedataapi.gameCharacters.exceptions.GameCharacterNotFoundException;
 import com.garfield.framedataapi.games.exceptions.GameAlreadyExistsException;
 import com.garfield.framedataapi.games.exceptions.GameNotFoundException;
+import com.garfield.framedataapi.users.exceptions.UserBannedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +14,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({AmbiguousCharacterNameException.class})
+    public ResponseEntity<ApiResponse<String>> handleAmbiguousCharacterNameException(AmbiguousCharacterNameException e) {
+        return ApiResponseEntity.error(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler({GameCharacterNotFoundException.class})
+    public ResponseEntity<ApiResponse<String>> handleGameCharacterNotFoundException(GameCharacterNotFoundException e) {
+        return ApiResponseEntity.error(HttpStatus.NOT_FOUND, e.getMessage());
+    }
 
     @ExceptionHandler({GameNotFoundException.class})
     public ResponseEntity<ApiResponse<String>> handleGameNotFoundException(GameNotFoundException e) {
@@ -20,6 +33,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({GameAlreadyExistsException.class})
     public ResponseEntity<ApiResponse<String>> handleGameAlreadyExistsException(GameAlreadyExistsException e) {
         return ApiResponseEntity.error(HttpStatus.CONFLICT, e.getMessage());
+    }
+
+    @ExceptionHandler({UserBannedException.class})
+    public ResponseEntity<ApiResponse<String>> handleUserBannedException(UserBannedException e) {
+        return ApiResponseEntity.error(HttpStatus.FORBIDDEN, e.getMessage());
     }
 
 }
