fcd04ac7127fc51307ab8b184650f80a8b1e3aa1
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-05 00:23:14 +0200

    create dto

--- src/main/java/com/garfield/framedataapi/framedata/dtos/CreateFramedataDto.java
+++ src/main/java/com/garfield/framedataapi/framedata/dtos/CreateFramedataDto.java
@@ -1,4 +1,12 @@
 package com.garfield.framedataapi.framedata.dtos;
 
-public record CreateFramedataDto() {
+import com.garfield.framedataapi.config.validation.ValidUuid;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.Map;
+import java.util.UUID;
+
+public record CreateFramedataDto(
+        @ValidUuid(message = "\"characterId\" field must be a valid UUID.") UUID characterId,
+        @NotEmpty(message = "\"attributes\"") Map<String, Object> attributes) {
 }
