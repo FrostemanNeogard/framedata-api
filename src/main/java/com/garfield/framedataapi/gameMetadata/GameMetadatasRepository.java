ca24295439d08e916fe602bd58f606a75312e937
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-05 00:21:22 +0200

    remove unnecessary game metadata module

--- src/main/java/com/garfield/framedataapi/gameMetadata/GameMetadatasRepository.java
+++
@@ -1,8 +0,0 @@
-package com.garfield.framedataapi.gameMetadata;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import java.util.UUID;
-
-public interface GameMetadatasRepository extends ListCrudRepository<GameMetadata, UUID> {
-}
