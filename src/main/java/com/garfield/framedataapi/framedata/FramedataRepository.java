7c251f3d7bc239d7eee4db8e269c7b015ef0dd0d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-05 00:23:24 +0200

    add findAllByCharacter method

--- src/main/java/com/garfield/framedataapi/framedata/FramedataRepository.java
+++ src/main/java/com/garfield/framedataapi/framedata/FramedataRepository.java
@@ -1,8 +1,11 @@
 package com.garfield.framedataapi.framedata;
 
+import com.garfield.framedataapi.gameCharacters.GameCharacter;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.Set;
 import java.util.UUID;
 
 public interface FramedataRepository extends ListCrudRepository<Framedata, UUID> {
+    Set<Framedata> findAllByGameCharacter(GameCharacter gameCharacter);
 }
