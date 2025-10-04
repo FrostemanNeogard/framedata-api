5f75d42a055be84366d294267d9480d3d6ac65be
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-05 00:23:06 +0200

    change attributes type and create fromEntity method

--- src/main/java/com/garfield/framedataapi/framedata/dtos/FramedataDto.java
+++ src/main/java/com/garfield/framedataapi/framedata/dtos/FramedataDto.java
@@ -1,8 +1,19 @@
 package com.garfield.framedataapi.framedata.dtos;
 
+import com.garfield.framedataapi.framedata.Framedata;
 import com.garfield.framedataapi.gameCharacters.dtos.GameCharacterDto;
 
+import java.util.Map;
 import java.util.UUID;
 
-public record FramedataDto(UUID id, GameCharacterDto gameCharacter, String attributes) {
+public record FramedataDto(UUID id, GameCharacterDto gameCharacter, Map<String, Object> attributes) {
+
+    public static FramedataDto fromEntityAndAttributesMap(Framedata framedata, Map<String, Object> attributesMap) {
+        return new FramedataDto(
+                framedata.getId(),
+                GameCharacterDto.fromEntity(framedata.getGameCharacter()),
+                attributesMap
+        );
+    }
+
 }
