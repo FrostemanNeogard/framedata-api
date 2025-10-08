70bb11126ef86034be55c201749060c833a4773b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-08 17:54:50 +0200

    add constructor for UUID

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataNotFoundException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataNotFoundException.java
@@ -2,7 +2,13 @@ package com.garfield.framedataapi.framedata.exceptions;
 
 import com.garfield.framedataapi.gameCharacters.GameCharacter;
 
+import java.util.UUID;
+
 public class FramedataNotFoundException extends RuntimeException {
+
+    public FramedataNotFoundException(UUID id) {
+        super(String.format("No framedata found for character: \"%s\".", id));
+    }
 
     public FramedataNotFoundException(GameCharacter gameCharacter, String input) {
         super(String.format(
