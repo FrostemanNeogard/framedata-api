a1b57b0772b24a75cbd1a232b710499a62ae17e9
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-22 18:34:50 +0200

    remove unused constructor

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/JsonFormatException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/JsonFormatException.java
@@ -1,15 +1,9 @@
 package com.garfield.framedataapi.framedata.exceptions;
 
-import java.util.Map;
-
 public class JsonFormatException extends RuntimeException {
 
     public JsonFormatException(String invalidJson) {
         super(String.format("Invalid JSON format: \"%s\".", invalidJson));
-    }
-
-    public JsonFormatException(Map<String, Object> invalidJson) {
-        this(invalidJson.toString());
     }
 
 }
