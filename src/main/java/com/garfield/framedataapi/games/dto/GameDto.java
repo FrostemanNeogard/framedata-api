0d1dd74a0fb4b225006fcb46af21c836c9934639
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-22 21:24:42 +0200

    include attributesTemplate in game dto

--- src/main/java/com/garfield/framedataapi/games/dto/GameDto.java
+++ src/main/java/com/garfield/framedataapi/games/dto/GameDto.java
@@ -1,11 +1,14 @@
 package com.garfield.framedataapi.games.dto;
 
+import com.garfield.framedataapi.framedata.FramedataAttributes;
 import com.garfield.framedataapi.games.Game;
 
 import java.util.UUID;
 
-public record GameDto(String name, UUID id) {
+public record GameDto(String name, UUID id, FramedataAttributes attributesTemplate) {
+
     public static GameDto fromEntity(Game game) {
-        return new GameDto(game.getName(), game.getId());
+        return new GameDto(game.getName(), game.getId(), game.getAttributesTemplate());
     }
+
 }
