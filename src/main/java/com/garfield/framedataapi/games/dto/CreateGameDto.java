be531f48c09d9f3a1cbfdb57e73988768509480c
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-22 18:36:39 +0200

    change map field to FramedataAttributes

--- src/main/java/com/garfield/framedataapi/games/dto/CreateGameDto.java
+++ src/main/java/com/garfield/framedataapi/games/dto/CreateGameDto.java
@@ -1,11 +1,10 @@
 package com.garfield.framedataapi.games.dto;
 
+import com.garfield.framedataapi.framedata.FramedataAttributes;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
-import java.util.Map;
-
 public record CreateGameDto(
-        @NotBlank(message = "\"name\" field is required.") String name,
-        @NotEmpty(message = "\"attributesTemplate\" field is required.") Map<String, Object> attributesTemplate) {
+        @NotBlank(message = "\"name\" field is required") String name,
+        @NotEmpty(message = "\"attributesTemplate\" field is required") FramedataAttributes attributesTemplate) {
 }
