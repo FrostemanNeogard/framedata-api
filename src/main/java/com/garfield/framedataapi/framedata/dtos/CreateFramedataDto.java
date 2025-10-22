cdb628e60efac70109fae2a530f4c7712b76c2b4
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-22 18:23:26 +0200

    change validation messages

--- src/main/java/com/garfield/framedataapi/framedata/dtos/CreateFramedataDto.java
+++ src/main/java/com/garfield/framedataapi/framedata/dtos/CreateFramedataDto.java
@@ -1,12 +1,11 @@
 package com.garfield.framedataapi.framedata.dtos;
 
-import com.garfield.framedataapi.config.validation.ValidUuid;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.Map;
 import java.util.UUID;
 
 public record CreateFramedataDto(
-        @ValidUuid(message = "\"characterId\" field must be a valid UUID.") UUID characterId,
-        @NotEmpty(message = "\"attributes\"") Map<String, Object> attributes) {
+        UUID characterId,
+        @NotEmpty(message = "\"attributes\" field is required.") Map<String, Object> attributes) {
 }
