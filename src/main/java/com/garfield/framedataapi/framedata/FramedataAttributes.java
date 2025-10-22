5b0f4050fe58a292b38cebdf0675f51c2a7a5aa4
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-22 22:14:47 +0200

    add constructors and reserved key logic

--- src/main/java/com/garfield/framedataapi/framedata/FramedataAttributes.java
+++ src/main/java/com/garfield/framedataapi/framedata/FramedataAttributes.java
@@ -2,26 +2,43 @@ package com.garfield.framedataapi.framedata;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.garfield.framedataapi.games.exceptions.InvalidAttributesTemplateJsonException;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@NoArgsConstructor
 public class FramedataAttributes {
 
     @NotNull(message = "Categories array must be present")
     @NotEmpty(message = "Categories array cannot be empty")
-    private List<String> categories;
+    private final List<String> categories;
 
     @NotNull(message = "Identifiers array must be present")
     @NotEmpty(message = "Identifiers array cannot be empty")
-    private List<String> identifiers;
+    private final List<String> identifiers;
 
-    private final Map<String, Object> dynamicFields = new HashMap<>();
+    private final Map<String, Object> dynamicFields;
+
+    public FramedataAttributes(Map<String, Object> dynamicFields) {
+        if (dynamicFields.containsKey("categories")) {
+            throw new InvalidAttributesTemplateJsonException("categories");
+        }
+
+        if (dynamicFields.containsKey("identifiers")) {
+            throw new InvalidAttributesTemplateJsonException("identifiers");
+        }
+
+        this.categories = List.of("");
+        this.identifiers = List.of("");
+        this.dynamicFields = dynamicFields;
+    }
+
+    public FramedataAttributes() {
+        this(new HashMap<>());
+    }
 
     @JsonAnySetter
     public void add(String key, Object value) {
