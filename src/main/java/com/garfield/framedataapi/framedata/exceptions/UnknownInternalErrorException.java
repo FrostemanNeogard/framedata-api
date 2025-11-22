17ed2da7bbf096e1b15507e96ae778ee29067f94
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-11-22 18:30:02 +0100

    add extra constructor for UnknownInternalErrorException

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/UnknownInternalErrorException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/UnknownInternalErrorException.java
@@ -2,6 +2,10 @@ package com.garfield.framedataapi.framedata.exceptions;
 
 public class UnknownInternalErrorException extends RuntimeException {
 
+    public UnknownInternalErrorException() {
+        super("Something went wrong. Please try again later.");
+    }
+
     public UnknownInternalErrorException(Exception e) {
         super(String.format("An unknown error occurred: \"%s\".", e.getMessage()));
     }
