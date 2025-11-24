e3260d655e8f53a1bc1156a40082ae867c783aab
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-11-25 00:34:29 +0100

    fix some error messages

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataJsonInvalidFieldTypeException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataJsonInvalidFieldTypeException.java
@@ -1,11 +1,9 @@
 package com.garfield.framedataapi.framedata.exceptions;
 
-import com.fasterxml.jackson.databind.node.JsonNodeType;
-
 public class FramedataJsonInvalidFieldTypeException extends RuntimeException {
 
-    public FramedataJsonInvalidFieldTypeException(String fieldName, JsonNodeType fieldType) {
-        super(String.format("\"%s\" has to be of type \"%s\".", fieldName, fieldType));
+    public FramedataJsonInvalidFieldTypeException(String fieldName, String fieldTypeName) {
+        super(String.format("\"%s\" has to be of type String. Found a type of \"%s\".", fieldName, fieldTypeName));
     }
 
 }
