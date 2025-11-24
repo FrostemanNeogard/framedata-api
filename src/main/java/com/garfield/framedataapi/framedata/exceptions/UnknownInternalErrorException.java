ebbb78b08583d556c38c25b05c887a32d266a509
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-11-25 00:34:32 +0100

    fix some error messages

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/UnknownInternalErrorException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/UnknownInternalErrorException.java
@@ -6,8 +6,12 @@ public class UnknownInternalErrorException extends RuntimeException {
         super("Something went wrong. Please try again later.");
     }
 
+    public UnknownInternalErrorException(String message) {
+        super(String.format("An unknown error occurred: \"%s\"", message));
+    }
+
     public UnknownInternalErrorException(Exception e) {
-        super(String.format("An unknown error occurred: \"%s\".", e.getMessage()));
+        super(String.format("An unknown error occurred: \"%s\"", e.getMessage()));
     }
 
 }
