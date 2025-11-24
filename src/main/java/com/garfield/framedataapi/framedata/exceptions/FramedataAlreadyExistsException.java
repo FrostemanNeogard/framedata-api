cc338c131dddac8aca097c0c654f8508a74eb48b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-11-25 00:33:55 +0100

    change error message template

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataAlreadyExistsException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataAlreadyExistsException.java
@@ -5,7 +5,7 @@ import com.garfield.framedataapi.framedata.Framedata;
 public class FramedataAlreadyExistsException extends RuntimeException {
 
     public FramedataAlreadyExistsException(Framedata framedata) {
-        super(String.format("Framedata already exists for attack: \"%s\".", framedata.getAttributes()));
+        super(String.format("Framedata already exists for this attack: \"%s\"", framedata.getData().toString()));
     }
 
 }
