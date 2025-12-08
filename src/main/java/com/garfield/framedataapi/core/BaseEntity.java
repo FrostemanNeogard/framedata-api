857aac90f9bd51831b72f6a8892e87d291535204
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-12-08 19:25:33 +0100

    add Getter to BaseEntity's ID field

--- src/main/java/com/garfield/framedataapi/core/BaseEntity.java
+++ src/main/java/com/garfield/framedataapi/core/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import java.util.UUID;
 
@@ -12,6 +13,7 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Getter
     protected UUID id;
 
 }
