55fe895afb71a3f58f4315e0c174c15c8545b8aa
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-12-08 19:25:20 +0100

    simplify GameCharacterDto by removing GameDto and changing field names

--- src/main/java/com/garfield/framedataapi/gameCharacters/dtos/GameCharacterDto.java
+++ src/main/java/com/garfield/framedataapi/gameCharacters/dtos/GameCharacterDto.java
@@ -1,17 +1,15 @@
 package com.garfield.framedataapi.gameCharacters.dtos;
 
 import com.garfield.framedataapi.gameCharacters.GameCharacter;
-import com.garfield.framedataapi.games.dto.GameDto;
 
 import java.util.UUID;
 
-public record GameCharacterDto(UUID characterId, String characterName, GameDto game) {
+public record GameCharacterDto(UUID id, String name) {
 
     public static GameCharacterDto fromEntity(GameCharacter gameCharacter) {
         return new GameCharacterDto(
                 gameCharacter.getId(),
-                gameCharacter.getName(),
-                GameDto.fromEntity(gameCharacter.getGame())
+                gameCharacter.getName()
         );
     }
 
