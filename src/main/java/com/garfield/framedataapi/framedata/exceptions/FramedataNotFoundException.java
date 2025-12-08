e12010a4c2d447b2df8ed1ed5e62cbd46d03786b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-12-08 19:25:57 +0100

    fix exception message

--- src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataNotFoundException.java
+++ src/main/java/com/garfield/framedataapi/framedata/exceptions/FramedataNotFoundException.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class FramedataNotFoundException extends RuntimeException {
 
     public FramedataNotFoundException(UUID id) {
-        super(String.format("No framedata found for character: \"%s\".", id));
+        super(String.format("No framedata found with ID: \"%s\".", id));
     }
 
     public FramedataNotFoundException(GameCharacter gameCharacter, String input) {
