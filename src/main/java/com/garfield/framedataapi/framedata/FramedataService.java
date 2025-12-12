1ae011dbd8967ca41550a6e42b3b66e4894b8d67
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-12-12 01:24:35 +0100

    initial framedata search implementation

--- src/main/java/com/garfield/framedataapi/framedata/FramedataService.java
+++ src/main/java/com/garfield/framedataapi/framedata/FramedataService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -97,9 +98,104 @@ public class FramedataService {
         this.framedataRepository.delete(framedata);
     }
 
-    // TODO: Temporary method from old implementation
-    // This should potentially be completely reworked
+    private record ScoredFramedata(Framedata framedata, double score) {
+    }
+
+    public Framedata getFramedataByInput(GameCharacter gameCharacter, String inputNotation) {
+        Set<Framedata> characterFramedata = gameCharacter.getFramedata();
+
+        String rawInput = inputNotation.replaceAll("[\u200B-\u200D\uFEFF]", "");
+        Optional<Framedata> rawMatch = characterFramedata.stream()
+                .filter(fd -> fd.getIdentity().getIdentifiers().contains(rawInput))
+                .findFirst();
+
+        if (rawMatch.isPresent()) {
+            return rawMatch.get();
+        }
+
+        for (int i = 0; i < 2; i++) {
+            boolean removePlus = (i > 0);
+            String formattedInput = this.formatNotation(inputNotation, removePlus);
+
+            for (Framedata moveData : characterFramedata) {
+                List<String> identifiers = moveData.getIdentity().getIdentifiers();
+
+                for (String identifier : identifiers) {
+                    String formattedIdentifier = this.formatNotation(identifier, removePlus);
+
+                    if (formattedInput.equals(formattedIdentifier)) {
+                        return moveData;
+                    }
+                }
+            }
+        }
+
+        throw new FramedataNotFoundException(gameCharacter, inputNotation);
+    }
+
+    public Set<Framedata> getMostSimilarFramedataEntries(GameCharacter gameCharacter, String inputNotation) {
+        Set<Framedata> characterFramedata = gameCharacter.getFramedata();
+        List<ScoredFramedata> similarityMap = new ArrayList<>();
+
+        for (Framedata moveData : characterFramedata) {
+            double bestScoreForMove = 0.0;
+
+            for (String identifier : moveData.getIdentity().getIdentifiers()) {
+                double score = this.calculateSimilarity(identifier, inputNotation);
+                if (score > bestScoreForMove) {
+                    bestScoreForMove = score;
+                }
+            }
+
+            similarityMap.add(new ScoredFramedata(moveData, bestScoreForMove));
+        }
+
+        return similarityMap.stream()
+                .sorted(Comparator.comparingDouble(ScoredFramedata::score).reversed())
+                .map(ScoredFramedata::framedata)
+                .distinct()
+                .limit(5)
+                .collect(Collectors.toSet());
+    }
+
+    private double calculateSimilarity(String s1, String s2) {
+        String longer = s1, shorter = s2;
+        if (s1.length() < s2.length()) {
+            longer = s2;
+            shorter = s1;
+        }
+        int longerLength = longer.length();
+        if (longerLength == 0) {
+            return 1.0;
+        }
+
+        int distance = getLevenshteinDistance(longer, shorter);
+
+        return (longerLength - distance) / (double) longerLength;
+    }
+
+    private int getLevenshteinDistance(String s1, String s2) {
+        int[] costs = new int[s2.length() + 1];
+        for (int j = 0; j < costs.length; j++)
+            costs[j] = j;
+        for (int i = 1; i <= s1.length(); i++) {
+            costs[0] = i;
+            int nw = i - 1;
+            for (int j = 1; j <= s2.length(); j++) {
+                int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]),
+                        s1.charAt(i - 1) == s2.charAt(j - 1) ? nw : nw + 1);
+                nw = costs[j];
+                costs[j] = cj;
+            }
+        }
+        return costs[s2.length()];
+    }
+
     public String formatNotation(String inputNotation, boolean removePlus) {
+        if (inputNotation == null) {
+            return "";
+        }
+
         String modifiedNotation = inputNotation.toLowerCase();
 
         if (!modifiedNotation.contains("fc")) {
@@ -142,25 +238,6 @@ public class FramedataService {
         }
 
         return modifiedNotation;
-    }
-
-    // TODO: Temporary method from old implementation
-    // This should be improved
-    public Framedata getFramedataByInput(
-            GameCharacter gameCharacter,
-            String inputNotation) {
-
-        Set<Framedata> characterFramedata = gameCharacter.getFramedata();
-
-        return characterFramedata
-                .stream()
-                .filter(fd -> fd.getIdentity().getIdentifiers().contains(inputNotation))
-                .findFirst().orElseThrow(() -> new FramedataNotFoundException(gameCharacter, inputNotation));
-    }
-
-    public Set<Framedata> getMostSimilarFramedataEntries(GameCharacter gameCharacter, String inputNotation) {
-        // TODO: Implement
-        return gameCharacter.getFramedata();
     }
 
 }
