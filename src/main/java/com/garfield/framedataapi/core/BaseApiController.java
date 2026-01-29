2e88fc0827d8b384552e9a3c1a7cbaa51b90d42b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2026-01-29 15:34:49 +0100

    add @CrossOrigin annotation to BaseApiController

--- src/main/java/com/garfield/framedataapi/core/BaseApiController.java
+++ src/main/java/com/garfield/framedataapi/core/BaseApiController.java
@@ -2,11 +2,13 @@ package com.garfield.framedataapi.core;
 
 import com.garfield.framedataapi.advice.authorization.Authenticated;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.net.URI;
 import java.util.UUID;
 
 @Authenticated
+@CrossOrigin
 public abstract class BaseApiController {
 
     @Value("${server.servlet.context-path}")
